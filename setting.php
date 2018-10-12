b68a3e01629748f6c9684e64f0261bc0349caf8e
thomas <tl263@njit.edu>
2018-10-12 19:01:34 -0400

    Update,added redirect to other pages

---
+++ setting.php
@@ -0,0 +1,50 @@
+<style>
+html {
+	background-image: url("IT490_V4.png");
+	background-repeat: no-repeat;
+	background-color: yellow;
+	}
+ul {
+    			list-style-type: none;
+   			margin: 0px;
+    			padding: 0;
+    			overflow: hidden;
+    			background-color: #959595;
+    			position: -webkit-sticky;
+			position: sticky;
+			top: 0;
+			width:100%;
+		}
+
+		li {
+    			float: left;
+
+		}
+
+		li a {
+    			display: block;
+    			color: black;
+    			text-align: center;
+    			padding: 14px 16px;
+    			text-decoration: none;
+			}
+		.nav {
+   			list-style: none;
+    			font-weight: bold;
+    			margin-bottom: 10px;
+    			width: 100%;
+    			text-align: center;
+    			background-color: #959595;
+    			height:38px;
+			}
+</style>
+<body>
+<div id = "nav">
+<ul>
+  <li><a href="/homepage.php">Home</a></li>
+  <li><a href="/Reddit_Interface.php">Reddit Interface</a></li>
+  <li><a href="/setting.php">Setting</a></li>
+  <li><a href="/logout.php">Log Out</a></li>
+</ul>
+</div>
+</body>
